ae2d44c1f61996ce5e8c273f6689933e754c7160
Elise Lee <eliselee2021@u.northwestern.edu>
2019-10-30 16:38:45 -0500

    Initial front-end implementation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import {StyleSheet, ScrollView, Text, View, Image} from 'react-native';
+import DialogInput from 'react-native-dialog-input'
+import SwipeCards from 'react-native-swipe-cards';
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.card}>
+        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+      </View>
+    )
+  }
+}
+
+class NoMoreCards extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.noMoreCards}>
+        <Text>No more new faces right now</Text>
+      </View>
+    )
+  }
+}
+
+const cards = [
+  {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+
+const cards2 = [
+  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: cards,
+      outOfCards: false,
+      isDialogVisible: false
+    }
+  }
+
+  handleYup = card => {
+    this.setState({isDialogVisible: true});
+  }
+
+  handleNope (card) {
+    console.log("nope")
+  }
+
+  cardRemoved (index) {
+    console.log(`The index is ${index}`);
+
+    let CARD_REFRESH_LIMIT = 3
+
+    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+
+      if (!this.state.outOfCards) {
+        console.log(`Adding ${cards2.length} more cards`)
+
+        this.setState({
+          cards: this.state.cards.concat(cards2),
+          outOfCards: true
+        })
+      }
+
+    }
+
+  }
+
+  sendInput = name => {
+    // Not yet implemented, send name and card ID to database
+    this.showDialog(false)
+  }
+
+  showDialog = (isShown) => {
+    this.setState({isDialogVisible: isShown});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SwipeCards
+          cards={this.state.cards}
+          loop={false}
+
+          renderCard={(cardData) => <Card {...cardData} />}
+          renderNoMoreCards={() => <NoMoreCards />}
+
+          showYup={false}
+          showNope={false}
+          showMaybe={false}
+
+          handleYup={this.handleYup}
+          handleNope={this.handleNope}
+          cardRemoved={this.cardRemoved.bind(this)}
+        />
+        <DialogInput isDialogVisible={this.state.isDialogVisible}
+            title={"Great!"}
+            message={"Please enter their name."}
+            hintInput ={""}
+            dialogStyle={styles.popup}
+            modalStyle={styles.popupBackground}
+            submitInput={ (inputText) => {this.sendInput(inputText)} }
+            closeDialog={ () => {this.showDialog(false)}}>
+        </DialogInput>
+      </View>
+    )
+  }
+}
+
+HomeScreen.navigationOptions = {
+  title: 'Momento',
+};
+
+const styles = StyleSheet.create({
+  popup: {
+    marginBottom: 150,
+  },
+
+  popupBackground: {
+    backgroundColor: 'rgba(52, 52, 52, 0.4)'
+  },
+
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  card: {
+    alignItems: 'center',
+    borderRadius: 15,
+    overflow: 'hidden',
+    borderColor: 'white',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    elevation: 1,
+  },
+  thumbnail: {
+    width: 300,
+    height: 450,
+  },
+  text: {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  noMoreCards: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
